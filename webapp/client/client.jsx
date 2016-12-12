13050cd17b40ad9eb09e6a96c243311fe036ade3
George Goldberg <george@gberg.me>
2016-12-12 12:48:04 +0000

    Fix eslint errors.

--- webapp/client/client.jsx
+++ webapp/client/client.jsx
@@ -1670,7 +1670,7 @@ export default class Client {
         }
 
         // Regex lifted from https://github.com/mattermost/marked/blob/master/lib/marked.js for GFM URL matching.
-        if (post.message.match(/((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s`!()\[\]{};:'".,<>?«»“”‘’]|[`!\[\]{};:'".,<>?«»“”‘’](?=[^\s()<>])|\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\))+)/i)) {
+        if (post.message.match(/((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s`!()[\]{};:'".,<>?«»“”‘’]|[`![\]{};:'".,<>?«»“”‘’](?=[^\s()<>])|\((?:[^\s()<>]|(?:\([^\s()<>]+\)))*\))+)/i)) {
             this.trackEvent('api', 'api_posts_links', {team_id: this.getTeamId(), channel_id: post.channel_id});
         }
 
--- webapp/components/more_direct_channels.jsx
+++ webapp/components/more_direct_channels.jsx
@@ -6,7 +6,6 @@ import SpinnerButton from 'components/spinner_button.jsx';
 
 import {searchUsers} from 'actions/user_actions.jsx';
 import {openDirectChannelToUser} from 'actions/channel_actions.jsx';
-import {trackEvent} from 'actions/diagnostics_actions.jsx';
 
 import UserStore from 'stores/user_store.jsx';
 import TeamStore from 'stores/team_store.jsx';
