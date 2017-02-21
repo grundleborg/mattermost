d2c97ac25a5889ac1df82251b0fad02dc43cd62b
George Goldberg <george@gberg.me>
2017-02-21 16:08:39 +0000

    Removed old identify code.

--- webapp/components/logged_in.jsx
+++ webapp/components/logged_in.jsx
@@ -26,7 +26,6 @@ export default class LoggedIn extends React.Component {
         super(params);
 
         this.onUserChanged = this.onUserChanged.bind(this);
-        this.setupUser = this.setupUser.bind(this);
 
         // Because current CSS requires the root tag to have specific stuff
         $('#root').attr('class', 'channel-view');
@@ -45,9 +44,7 @@ export default class LoggedIn extends React.Component {
             user: UserStore.getCurrentUser()
         };
 
-        if (this.state.user) {
-            this.setupUser(this.state.user);
-        } else {
+        if (!this.state.user) {
             GlobalActions.emitUserLoggedOutEvent('/login');
         }
     }
@@ -56,41 +53,10 @@ export default class LoggedIn extends React.Component {
         return this.state.user != null;
     }
 
-    setupUser(user) {
-        // Update segment indentify
-        if (global.window && global.window.analytics) {
-            if (global.window.mm_config.SegmentDeveloperKey != null && global.window.mm_config.SegmentDeveloperKey !== '') {
-                // Segment for Analytics.
-                global.window.analytics.identify(user.id, {
-                    createdAt: user.create_at,
-                    id: user.id
-                });
-            } else {
-                // Segment for Diagnostics.
-                global.window.analytics.identify(user.id, {
-                    createdAt: user.create_at,
-                    id: user.id
-                }, {
-                    context: {
-                        ip: '0.0.0.0'
-                    },
-                    page: {
-                        path: '',
-                        referrer: '',
-                        search: '',
-                        title: '',
-                        url: ''
-                    }
-                });
-            }
-        }
-    }
-
     onUserChanged() {
         // Grab the current user
         const user = UserStore.getCurrentUser();
         if (!Utils.areObjectsEqual(this.state.user, user)) {
-            this.setupUser(user);
             this.setState({
                 user
             });
